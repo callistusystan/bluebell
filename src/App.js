d78d37189716322972a57fcd243b04eaecffc96f
Kai Yang Lim <kai.yang@getflip.com.au>
2022-08-21 01:41:28 +1000

    stamp collection page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
-import HomePage from './pages/HomePage';
-import ProfilePage from './pages/ProfilePage';
-import MobileHackathon from './components/react-mobile-hackathon';
+import React, { Component } from "react";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import MobileHackathon from "./components/react-mobile-hackathon";
 import { Route, Switch } from "react-router-dom";
+import StampCollectionPage from "./pages/StampCollectionPage";
 
 class App extends Component {
-    render() {
-        return (
-            <MobileHackathon>
-              <Switch>
-                <Route path="/profile" component={ProfilePage} />
-                <Route exact path="/" component={HomePage} />
-              </Switch>
-            </MobileHackathon>
-        );
-    }
+  render() {
+    return (
+      <MobileHackathon>
+        <Switch>
+          <Route path="/profile" component={ProfilePage} />
+          <Route path="/stamp-collection" component={StampCollectionPage} />
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </MobileHackathon>
+    );
+  }
 }
 
 export default App;
