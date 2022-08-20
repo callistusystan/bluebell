997da35b3b3b79e5d3ffd3d54edd53042daa3db1
Callistus Tan <callistusystan@gmail.com>
2022-08-21 02:43:41 +1000

    Implemented lake page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import HomePage from './pages/HomePage';
 import ProfilePage from './pages/ProfilePage';
+import LakePage from './pages/LakePage';
 import MobileHackathon from './components/react-mobile-hackathon';
 import { Route, Switch } from "react-router-dom";
 
@@ -10,6 +11,7 @@ class App extends Component {
             <MobileHackathon>
               <Switch>
                 <Route path="/profile" component={ProfilePage} />
+                <Route path="/lake" component={LakePage} />
                 <Route exact path="/" component={HomePage} />
               </Switch>
             </MobileHackathon>
